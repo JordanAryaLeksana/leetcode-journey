223b2d479982f4aaa0e18f7c7bd418bde58a5f77
Jordan Arya Leksana <jordanaryaleksana41@gmail.com>
2026-06-07 21:10:39 +0700

    Time: 3 ms (88.85%) | Memory: 19.2 MB (53.64%) - LeetSync

--- 9-palindrome-number/palindrome-number.py
+++ 9-palindrome-number/palindrome-number.py
@@ -5,7 +5,10 @@ class Solution:
 
         while left <= right:
             if s[left] != s[right]:
-                return False 
+                return False
             left += 1
             right -= 1
-        return True 
+        return True
+        
+
+        
