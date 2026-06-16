e2f0e688474705e7f4e9ae8b0609474b6271db76
Jordan Arya Leksana <jordanaryaleksana41@gmail.com>
2026-06-16 23:02:03 +0700

    Time: 5 ms (71.39%), Space: 19.2 MB (86.45%) - LeetHub

--- 0009-palindrome-number/0009-palindrome-number.py
+++ 0009-palindrome-number/0009-palindrome-number.py
@@ -3,12 +3,9 @@ class Solution:
         s = str(x)
         left, right = 0, len(s) - 1
 
-        while left <= right:
+        while left <= right: 
             if s[left] != s[right]:
                 return False
             left += 1
             right -= 1
         return True
-        
-
-        
